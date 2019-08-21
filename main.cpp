46f0d94cc67c5d4a02711405ffb27a62c37243ef
drasis <doron.rasis@nyu.edu>
2019-08-21 17:12:11 -0400

    very nice save line jpeg awesome great

--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
-#include <string>
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
 #include <iostream>
-#include <opencv2/opencv.hpp>
 
-// using namespace td;
+// void saveEdgeDetectedImage(cv::Mat pic, std::string filename="./edges.jpg") {
+//   cv::imwrite(filename, pic);
+// }
 
-int main() {
-	std::cout << "hi\n";
+cv::Mat getEdges(cv::Mat pic, int threshold=20, int ratio=3, int kernel_size=3) {
+  cv::Mat detected_edges;
+  cv::Mat gray;
+  cv::cvtColor(pic, gray, cv::COLOR_BGR2GRAY);
+  cv::blur(gray, detected_edges, cv::Size(3,3));
+  cv::Canny(detected_edges, detected_edges, threshold, threshold*ratio, kernel_size);
+  return detected_edges;
 }
 
+int main( int argc, char** argv ) {
+  cv::CommandLineParser parser( argc, argv, "{@input | ../data/fruits.jpg | input image}" );
+  cv::Mat src = cv::imread( parser.get<cv::String>( "@input" ), cv::IMREAD_COLOR ); // Load an image
+  if( src.empty() )
+  {
+    std::cout << "Could not open or find the image!\n" << std::endl;
+    std::cout << "Usage: " << argv[0] << " <Input image>" << std::endl;
+    return -1;
+  }
+  cv::Mat edges = getEdges(src);
+  cv::imwrite("./edges.jpg", edges);
+  std::cout << "done" << "\n";
+  return 0;
+}
